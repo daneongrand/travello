fd1f28183cfae73aeeaf19ea08ae4c72aaaac970
daneongrand <danektolstov@gmail.com>
2021-10-30 19:47:10 +0400

    Rework logic carousel.js, add second carousel into layout and her styles

--- src/index.js
+++ src/index.js
@@ -3,20 +3,25 @@ import carousel from './js/carousel'
 
 window.addEventListener('load', () => {
 
-    const itemWidth = window.getComputedStyle(document.querySelector('.offer-section-carousel-container-content-item')).minWidth.slice(0, window.getComputedStyle(document.querySelector('.offer-section-carousel-container-content-item')).minWidth.indexOf('px'))
-    const widthContainer = window.getComputedStyle(document.querySelector('.offer-section-carousel-container-content')).width.slice(0, window.getComputedStyle(document.querySelector('.offer-section-carousel-container-content')).width.indexOf('px'))
-    
+
     carousel({
-        widthContainer: widthContainer,
-        nextBtnId: 'next-button',
-        prevBtnId: 'prev-button',
-        itemsSelector: '.offer-section-carousel-container-content-item',
-        itemWidth: +itemWidth,
-        itemsToScroll: 1,
-        itemsToShow: 3,
-        margin: 20
+        countItemsMove: 1,
+        carouselTrackSelector: '.offer-section-carousel-container-content',
+        carouselItemsSelector: '.offer-section-carousel-container-content-item',
+        moveTransition: '.4s',
+        buttons: {
+            prev: 'prev-button',
+            next: 'next-button'
+        }
     })
 
+
+    carousel({
+        countItemsMove: 1,
+        carouselTrackSelector: '.flash-deals-section-wrapper-container-track',
+        carouselItemsSelector: '.flash-deals-section-wrapper-container-track-item',
+        moveTransition: '.4s'
+    })
 
 })
 
--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,65 +1,122 @@
-export default function carousel( configuration ) {
-    // console.log(configuration.itemWidth)
-    const nextBtn = document.getElementById(configuration.nextBtnId)
-    const prevBtn = document.getElementById(configuration.prevBtnId)
-    const items = document.querySelectorAll(configuration.itemsSelector)
-    const movePosition = configuration.itemsToScroll * configuration.itemWidth
+export default function carousel(options) {
+    const listItem = document.querySelectorAll(options.carouselItemsSelector)
+    const listWidthItem = []
+    const listMarginLeftItem = []
+    const listMarginRightItem = []
+    if(listItem) {
+        for(let item of listItem) {
+            listWidthItem.push(+window.getComputedStyle(item).width.slice(0, window.getComputedStyle(item).width.indexOf('px')))
+        }
+        for(let item of listItem) {
+            listMarginLeftItem.push(+window.getComputedStyle(item).marginLeft.slice(0, window.getComputedStyle(item).marginLeft.indexOf('px')))
+        }
+        for(let item of listItem) {
+            listMarginRightItem.push(+window.getComputedStyle(item).marginRight.slice(0, window.getComputedStyle(item).marginRight.indexOf('px')))
+        }
+        setTransition(listItem, options.moveTransition)
+    }
+    const track = document.querySelector(options.carouselTrackSelector)
+    const widthTracker = window.getComputedStyle(document.querySelector(options.carouselTrackSelector)).width.slice(0,window.getComputedStyle(document.querySelector(options.carouselTrackSelector)).width.indexOf('px'))
+    const widthListItem = listWidthItem.reduce((acc, sum) => acc += sum, 0) + listMarginLeftItem.reduce((acc, sum) => acc += sum, 0) + listMarginRightItem.reduce((acc, sum) => acc += sum, 0)
+    const countItemsMove = options.countItemsMove
+    const movePosition = countItemsMove * (listWidthItem[0] + listMarginLeftItem[0] + listMarginRightItem[0])
     let position = 0
-    const itemToShow = Math.floor(configuration.widthContainer / configuration.itemWidth)
-    const margin = ( configuration.widthContainer - configuration.itemWidth * itemToShow ) / ( itemToShow * 2 )
-    const rigthLimit = ( configuration.itemWidth + 2 * margin ) * +items.length - ( configuration.itemWidth + 2 * margin ) * itemToShow
-    
-    
-    console.log('rigthLimit' + rigthLimit)
 
 
-    for(let i = 0; i < items.length; i++) {
-        items[i].style.marginLeft = margin + 'px'
-        items[i].style.marginRight = margin + 'px'
-    }
-
-
-
-    const setPosition = () => {
-        for(let i of items) {
-            i.style.transform = `translateX(${position}px)`
-        }
-    }
-
-    const checkPositonRight = (p, mp) => {
-        console.log(Math.abs(p - mp))
-        if (Math.abs(p - mp) > rigthLimit) {
-            return rigthLimit + p
-        }
-        
-        return mp
-    }
-
-    const checkPositonLeft = (p, mp) => {
-        if ((p + mp) > 0) {
-            return 0 - p
-        }
-        return mp
-    }
-
-    // console.log('res' + checkPositon(-2630, 200))
-
-    nextBtn.addEventListener('click', () => {
-        position -= checkPositonRight(position, movePosition)
-        // console.log(Math.abs(position - movePosition))
-        setPosition()
-    })
-
-    prevBtn.addEventListener('click', () => {
-        position += checkPositonLeft(position, movePosition)
-        setPosition()
-    })
-
-    for (let i of items) {
-        i.addEventListener('mousedown', (event) => {
-            console.log(event.clientX)
+    if (options.buttons?.prev && options.buttons?.next) {
+        const btnPrev = document.getElementById(options.buttons.prev) 
+        const btnNext = document.getElementById(options.buttons.next) 
+        btnPrev.addEventListener('click', () => {
+            console.log(position)
+            position += checkPositionLeft() 
+            for (let item of listItem) {
+                item.style.transform = `translate(${position}px)`
+            } 
+        })
+        btnNext.addEventListener('click', () => {
+            console.log(position)
+            position -= checkPositionRigth()
+            for (let item of listItem) {
+                item.style.transform = `translate(${position}px)`
+            }       
+            
         })
     }
+    
 
+    track.addEventListener('mousedown', (e) => {
+        e.preventDefault()
+
+        setTransition(listItem, '')
+
+        let x = e.pageX
+
+        document.addEventListener('mousemove', moveItems)
+
+        document.addEventListener('mouseup', () => {
+            x = 0
+            document.removeEventListener('mousemove', moveItems)
+            position = +listItem[0].style.transform.slice(listItem[0].style.transform.indexOf('(') + 1, listItem[0].style.transform.indexOf('px'))
+            setTransition(listItem, options.moveTransition)
+        })
+        
+        function moveItems(e) {
+            e.preventDefault()
+            if (position-(x-e.pageX) > 0) return
+            if (position-(x-e.pageX) < -(widthListItem - widthTracker)) return
+            for(let item of listItem) {
+                item.style.transform = `translate(${position-(x-e.pageX)}px)`
+            }
+        }
+    })
+    
+    
+    // track.addEventListener('touchstart', (e) => {
+    //     e.preventDefault()
+
+    //     setTransition(listItem, '')
+
+    //     let x = e.changedTouches[0].clientX
+
+    //     document.addEventListener('touchmove', moveItems)
+
+    //     track.addEventListener('touchend', () => {
+    //         x = 0
+    //         document.removeEventListener('mousemove', moveItems)
+    //         position = +listItem[0].style.transform.slice(listItem[0].style.transform.indexOf('(') + 1, listItem[0].style.transform.indexOf('px'))
+    //         setTransition(listItem, options.moveTransition)
+    //     })
+        
+    //     function moveItems(e) {
+    //         e.preventDefault()
+    //         console.log(position)
+    //         if (position-(x-e.changedTouches[0].clientX) > 0) return
+    //         if (position-(x-e.changedTouches[0].clientX) < -(widthListItem - widthTracker)) return
+    //         for(let item of listItem) {
+    //             item.style.transform = `translate(${position-(x-e.changedTouches[0].clientX)}px)`
+    //         }
+    //     }
+    // })
+
+
+    function setTransition(listItem, transition) {
+        for(let item of listItem) {
+            item.style.transition = transition
+        }
+    }
+    
+    function checkPositionRigth() {
+        if (position - movePosition < -(widthListItem - widthTracker)) {
+            return position + (widthListItem - widthTracker)
+        } else return movePosition
+    }
+
+
+    function checkPositionLeft() {
+        if (position + movePosition > 0) {
+            return -position
+        } else return movePosition
+    }
 
 }
+
